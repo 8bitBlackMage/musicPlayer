2ba626116ec1b4957bc38c61ad31686cdca6fa30
aliceexley <aliceexley34@gmial.com>
2024-06-23 16:35:49 +0100

    MusicPlayer: added missing files

---
+++ MusicPlayer/TrackListBoxModel.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Alice Exley on 22/06/2024.
+//
+
+#include "TrackListBoxModel.h"
+#include "juce_gui_basics/juce_gui_basics.h"
+
+class TrackListBoxModel : public juce::TableListBoxModel
+{
+public:
+    TrackListBoxModel();
+
+};
---
+++ MusicPlayer/TrackListBoxModel.h
@@ -0,0 +1,14 @@
+//
+// Created by Alice Exley on 22/06/2024.
+//
+
+#ifndef GUI_APP_EXAMPLE_TRACKLISTBOXMODEL_H
+#define GUI_APP_EXAMPLE_TRACKLISTBOXMODEL_H
+
+
+class trackListBoxModel {
+
+};
+
+
+#endif //GUI_APP_EXAMPLE_TRACKLISTBOXMODEL_H
